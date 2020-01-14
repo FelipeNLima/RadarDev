e1a70e00baaf13bf1016931758b6dfb23391996a
Felipe <felipinho.lima27@gmail.com>
2020-01-14 15:18:15 -0300

    Projeto Back-End

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("../src/routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://felipe:felipe@cluster0-4f56k.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+//Métodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parâmetros:
+//Query Params: request.query Filtros, ordenação, paginação, ...
+//Route Params: request.params Identificar um recurso na alteração ou remoção
+//Body request.body Dados para criação ou alteração de um registro
